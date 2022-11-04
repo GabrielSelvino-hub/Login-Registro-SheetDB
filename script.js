41866bfd8e7f6a448f5e0f3e460e466e593db12a
GabrielSelvino-hub <65677195+GabrielSelvino-hub@users.noreply.github.com>
2022-11-03 22:12:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 
+var url = 'https://sheetdb.io/api/v1/30v73rsmrq5p2';
+var cop=0;
 
-function salvar() {
-    var name = document.querySelector('.form').children[1].children[0].value;
-    var email = document.querySelector('.form').children[2].children[0].value;
-    var pas = document.querySelector('.form').children[3].children[0].value;
-
-    axios.post('https://sheetdb.io/api/v1/30v73rsmrq5p2', {
+function salvar(name, emai, pas) {  
+    return axios.post(url, {
         "data": {
             "username": name,
-            "email": email,
+            "email": emai,
             "password": pas
         }
-    });
-    alert('Conta criada');
-    window.location.href = '/index.html';
+    }).then(x=>{window.location.href = '/index.html';});
 }
 
+function vfemail() {
+    axios.get(url,
+    ).then( response => {
+        var coletar;
+        coletar = response.data;
+        for (var i = 0;i < coletar.length;i++) {
+            var emai = document.querySelector('.form').children[2].children[0].value;
+            var name = document.querySelector('.form').children[1].children[0].value;
+            var pas = document.querySelector('.form').children[3].children[0].value;
+            if(coletar[i].email == emai){
+                cop = 1;
+                break;
+            } else {
+                if (cop == 0){
+                    cop = 2;    
+                } 
+            }
+        }
+        if(cop==1) {
+            alert('Esse email já existe')
+            location.reload();
+        }
+        if(cop==2) {
+            salvar(name, emai, pas);
+        }
+    })
+}
 
 function coletar() {
-    axios.get(`https://sheetdb.io/api/v1/30v73rsmrq5p2`,
+    axios.get(url,
     ).then( response => {
         var coletar;
         var res = false;
@@ -40,7 +63,7 @@ function coletar() {
 
 const spost = (event) => {
     event.preventDefault();
-    salvar();
+    vfemail();
 }
 const sget = (event) => {
     event.preventDefault();
@@ -50,7 +73,6 @@ const sget = (event) => {
 if (document.querySelector('.form').childElementCount == 5){
     document.querySelector('form').addEventListener('submit', sget);
 } else {
-    
     document.querySelector('form').addEventListener('submit', spost);
 }
 
